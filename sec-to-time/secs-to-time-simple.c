5e2106d7378d6f282c3ad62d091507937d89dc3f
Brian-ED <brianellingsgaard9@gmail.com>
2024-09-12 07:30:15 +0100

    fixed unfinished simpler version

--- sec-to-time/secs-to-time-simple.c
+++ sec-to-time/secs-to-time-simple.c
@@ -6,20 +6,13 @@
 
 int main(){
   double s;
-  printf("When inputing seconds, give 1 number and press enter. For example:\n");
-  printf("-----\n");
-  printf("21312\n");
-  printf("-----\n");
   printf("Input amount of seconds to translate to weeks, days, hours, minutes, and seconds:\n");
   scanf("%lf", &s);
 
-  printf("%ld %d %d %d %lf\n",
-    (long)(s/(7*24*60*60)),
-  printf("%ld %d %d %d %lf\n",
-    (int)fmod(s/(7*24*60*60),7),
-  printf("%ld %d %d %d %lf\n",
-    (int)fmod(s/(60*60),24),
-  printf("%d\n",(int)fmod(s/60,60))
+  printf("%ld weeks\n", (long)(s/(7*24*60*60)));
+  printf("%d\n",(int)fmod(s/(7*24*60*60),7));
+  printf("%d\n",(int)fmod(s/(60*60),24));
+  printf("%d\n",(int)fmod(s/60,60));
   printf("%lf\n",fmod(s,60));
 
   return 0;
