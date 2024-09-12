b7e9885d055484cccc0738e902b19a6d437075ce
Brian-ED <brianellingsgaard9@gmail.com>
2024-09-12 11:32:39 +0100

    added compilation for other platforms also renamed some release files

--- sec-to-time/compile.sh
+++ sec-to-time/compile.sh
@@ -1,2 +1,2 @@
-clang -O1 -Wall -std=c99 -Wno-missing-braces secs-to-time-big.c -o secs-to-time-big -lm
-clang -O1 -Wall -std=c99 -Wno-missing-braces secs-to-time-simple.c -o secs-to-time-simple -lm
+clang -O1 -Wall -std=c99 -Wno-missing-braces secs-to-time-big.c -o seconds-to-time-big -lm
+clang -O1 -Wall -std=c99 -Wno-missing-braces secs-to-time-simple.c -o seconds-to-time-simple -lm
