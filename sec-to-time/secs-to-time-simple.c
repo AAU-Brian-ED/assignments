873b5f45fe152ce864227c5d727fbbdd63c42beb
Brian-ED <brianellingsgaard9@gmail.com>
2024-09-12 09:57:09 +0100

    a lot more pretty. My precious diamond!

--- sec-to-time/secs-to-time-simple.c
+++ sec-to-time/secs-to-time-simple.c
@@ -1,6 +1,3 @@
-// seconds to weeks, days, hours, minutes, seconds
-// deadline 17 september 2024 kl 21:00
-
 #include <stdio.h>
 #include <math.h>
 
@@ -9,11 +6,12 @@ int main(){
   printf("Input amount of seconds to translate to weeks, days, hours, minutes, and seconds:\n");
   scanf("%lf", &s);
 
-  printf("%ld weeks\n", (long)(s/(7*24*60*60)));
-  printf("%d\n",(int)fmod(s/(7*24*60*60),7));
-  printf("%d\n",(int)fmod(s/(60*60),24));
-  printf("%d\n",(int)fmod(s/60,60));
-  printf("%lf\n",fmod(s,60));
+  //      text output      type            conversion     max number
+  printf("%ld weeks\n"  , (long  ) fmod(s/(1*60*60*24*7), INFINITY));
+  printf("%d days\n"    , (int   ) fmod(s/(1*60*60*24  ), 7       ));
+  printf("%d hours\n"   , (int   ) fmod(s/(1*60*60     ), 24      ));
+  printf("%d minutes\n" , (int   ) fmod(s/(1*60        ), 60      ));
+  printf("%lf seconds\n", (double) fmod(s/(1           ), 60      ));
 
   return 0;
 }
