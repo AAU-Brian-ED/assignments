72c4f9a4918f9580e00364974e8291ff4c3f946a
Brian-ED <brianellingsgaard9@gmail.com>
2024-09-12 09:56:38 +0100

    now code errors properly and includs more comments

--- sec-to-time/secs-to-time.c
+++ sec-to-time/secs-to-time.c
@@ -1,32 +1,73 @@
 // Seconds to weeks, days, hours, minutes, seconds.
 // Deadline 17 september 2024 kl 21:00.
 
+// PREREQUISITS:
+//   Please turn off inlay hints while veiwing this file. This is to allign code.
+//   Please use a monospaced font.
+//   I love using allignment to aid readability.
+
+
+// Exmplanation of how we convert seconds to minutes
+/*
+  We get the amount of seconds as input, and want to convert it to minutes.
+  For example, lets take 66.
+
+  Math:        || Explanation:
+  60s = m      // There are 60 seconds in one minute.
+  66s = x m    // We have 66 seconds, and that is equal to some amount of minutes.
+  66s = x 60s  // We can replace m with 60s because m = 60s.
+  66s/60s = x  // We can divide 60s on both sides of the = sign.
+  66/60 = x    // We devide by s on both the bottom and top side of the / symbol.
+
+  The answer is that 66 seconds is 66/60 minutes! :D
+
+  Simply showing minutes and seconds is wrong.
+  If we did, the output would be 66 seconds and 1.2
+*/
+
+
+// ┌──────────────────────────────────┐ //
+// │ Macro definitions are under here │ //
+// └──────────────────────────────────┘ //
+
 #include <stdio.h>
 #include <math.h>
 
-#define DAYS_IN_WEEK      7
-#define HOURS_IN_DAY      24
-#define MINUTES_IN_HOUR   60
-#define SECONDS_IN_MINUTE 60
+#define SECONDS_IN_MINUTE 60.0
+#define MINUTES_IN_HOUR   60.0
+#define HOURS_IN_DAY      24.0
+#define DAYS_IN_WEEK      7.0
+#define WEEKS_IN_UNIVERSE INFINITY
 
-#define WEEKS_TO_SECONDS   (DAYS_IN_WEEK    * DAYS_TO_SECONDS   )
-#define DAYS_TO_SECONDS    (HOURS_IN_DAY    * HOURS_TO_SECONDS  )
-#define HOURS_TO_SECONDS   (MINUTES_IN_HOUR * MINUTES_TO_SECONDS)
-#define MINUTES_TO_SECONDS SECONDS_IN_MINUTE
+#define SECONDS_TO_SECONDS 1.0
+#define MINUTES_TO_SECONDS (SECONDS_IN_MINUTE * SECONDS_TO_SECONDS)
+#define HOURS_TO_SECONDS   (MINUTES_IN_HOUR   * MINUTES_TO_SECONDS)
+#define DAYS_TO_SECONDS    (HOURS_IN_DAY      * HOURS_TO_SECONDS  )
+#define WEEKS_TO_SECONDS   (DAYS_IN_WEEK      * DAYS_TO_SECONDS   )
+
+
+// ┌──────────────────────┐ //
+// │ Types are under here │ //
+// └──────────────────────┘ //
 
 typedef double Seconds;
 
 typedef struct {
-  long int weeks;
+  long unsigned int weeks;
   unsigned char days, hours, minutes;
   double seconds;
 } Time;
+
+
+// ┌──────────────────────────┐ //
+// │ Functions are under here │ //
+// └──────────────────────────┘ //
 
 // Asks the user to give a valid amount of seconds as input
 Seconds AskForSeconds() {
   double x;
   if (1 != scanf("%lf", &x)) {
-    printf("Invalid number, defaulting to 0");
+    x = -1;
   }
   return x;
 }
@@ -34,27 +75,34 @@ Seconds AskForSeconds() {
 // Converts seconds to the Time type
 Time SecondsToTime(Seconds s) {
   Time result = {
-         s/WEEKS_TO_SECONDS                      ,
-    fmod(s/DAYS_TO_SECONDS   , DAYS_IN_WEEK     ),
-    fmod(s/HOURS_TO_SECONDS  , HOURS_IN_DAY     ),
-    fmod(s/MINUTES_TO_SECONDS, MINUTES_IN_HOUR  ),
-    fmod(s                   , SECONDS_IN_MINUTE),
+  //       ┌─Conversion factor to convert seconds to the time
+  //       │
+  //       │                   ┌─Using fmod, limit the size of the output.
+  //       │                   ├─They need to fit in their respective timeframe.
+  //       │                   ├─I.e 7 days would be 0 weeks, not 7 weeks.
+    fmod(s/WEEKS_TO_SECONDS  , WEEKS_IN_UNIVERSE), // Weeks
+    fmod(s/DAYS_TO_SECONDS   , DAYS_IN_WEEK     ), // Days
+    fmod(s/HOURS_TO_SECONDS  , HOURS_IN_DAY     ), // Hours
+    fmod(s/MINUTES_TO_SECONDS, MINUTES_IN_HOUR  ), // Minutes
+    fmod(s/SECONDS_TO_SECONDS, SECONDS_IN_MINUTE), // Seconds
   };
   return result;
 }
 
 // Prints the Time type
 void PrintTime(Time t) {
-  printf("%ld weeks.\n"  , t.weeks  );
+  printf("%lu weeks.\n"  , t.weeks  );
   printf("%d days.\n"    , t.days   );
   printf("%d hours.\n"   , t.hours  );
   printf("%d minutes.\n" , t.minutes);
   printf("%lf seconds.\n", t.seconds);
 }
 
-// |--------------------------| //
-// | Main program starts here | //
-// |--------------------------| //
+
+// ┌──────────────────────────┐ //
+// │ Main program starts here │ //
+// └──────────────────────────┘ //
+
 int main(int argc, char *argv[]){
 
   // The only variable we need is s, since we only ask for seconds
@@ -81,6 +129,10 @@ int main(int argc, char *argv[]){
     printf("-----\n");
     printf("Input amount of seconds to translate to weeks, days, hours, minutes, and seconds:\n");
     s = AskForSeconds();
+    if (s == -1) {
+      printf("ERROR: Invalid number\n");
+      return -1;
+    }
   }
 
   // Prints the time after converting seconds to time.
